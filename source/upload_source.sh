6a2052351c55c50bb10dd00e5289b7035c045f59
Owolabi sulaimon <owolabisulaimon90@gmail.com>
2022-10-31 09:43:11 +0100

    local host

---
+++ source/upload_source.sh
@@ -0,0 +1,5 @@
+mkdr fancyApi
+
+cd jobapi
+
+zip ../fancyApi/source.zip requirements.txt
---
+++ source/worker.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI,HTTPException
+import uvicorn
+app = FastAPI()
+
+jobs = [{
+        "id": "uniqueId1",
+        "title": "accountant",
+        "description": "financial decisions"
+    },
+    {
+        "id": "uniqueId2",
+        "title": "housekeeper",
+        "description": "cleans and sanitizes"
+    },
+    {
+        "id": "uniqueId3",
+        "title": "programmer",
+        "description": "program management"
+    }]
+
+
+@app.get("/job")
+def getJobs():
+    return jobs
+    
+@app.get("/job/{id}")
+def getJob(id):
+    for job in jobs:
+        if job["id"] == id:
+            return job
+    raise HTTPException(status_code=404, detail="Job with id" + id + "not found")
+
+    if _name_ =="_worker_":
+        uvicorn.run(app, host="0.0.0.0", port=80)
+
+
+
